27044a7c9148e8a32026c928136e5189913ff840
Roobre <roobre@roobre.es>
2014-06-05 19:07:49 +0200

    Modified example according to latest changes.

--- example.php
+++ example.php
@@ -14,15 +14,18 @@ $api->request('summoner/by-name/roobre', '1.3');
 print_r($api->response());
 
 
+if ($api->response()->code != '200') {
+	die('An error occurred while performing the request (status code: ' . $api->response()->code . ")\n");
+}
 // Store my id
 $id = $api->response()->body->roobre->id;
 
 // Request Teams
 $api->request("team/by-summoner/$id", '2.2');
 
-// Print the response
-//print_r($api->response());
-
+if ($api->response()->code != '200') {
+	die('An error occurred while performing the request (status code: ' . $api->response()->code . ")\n");
+}
 
 // Static example request
 $api->requestStaticData('summoner-spell');
